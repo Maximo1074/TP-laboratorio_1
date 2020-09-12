512e87424fb606d1fff5b7a9be650cd005b6aa1a
maximo1074 <maximiliano.augusto.arias@gmail.com>
2020-09-12 17:52:42 -0300

    Trabajo Calculadora

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "funciones.h"
+/** \brief en la funcion sumarOperandos se toma los dos valores enteros para sumar.
+ *
+ * \param int operandoA.
+ * \param int operandoB.
+ * \return devuelve el resultado de la suma.
+ *
+ */
+/** \brief en la funcion restarOperandos toma los dos valores enteros para restar.
+ *
+ * \param int operandoA.
+ * \param int operandoB.
+ * \return devuelve el resultado de la resta.
+ *
+ */
+ /** \brief en la funcion multiplicacionOperando toma los dos valores enteros para multiplicar.
+  *
+  * \param int operandoA.
+  * \param int operandoB.
+  * \return devuelve el resultado de la multiplicacion
+  *
+  */
+  /** \brief en la funcion divisionDeOperandos se toma los dos valores enteros para dividir.
+   *
+   * \param int operandoA.
+   * \param int operandoB.
+   * \return devuelve el resultado de la division en entero o flotante o si un numero se divide por cero envia un mensaje.
+   *
+   */
+   /** \brief en funcion factorialOperandos toma los valores enteros de los dos valores.
+    *
+    * \param int operandoA.
+    * \param int operandoB.
+    * \return devuelve el resultado factorial de los dos operandos.
+    *
+    */
+
 
 int main()
 {
